3f5c7a6fc04002f77509df59f43dc1ee7a3039b7
Phuong. Pham Thi - CMC Global DU3.21 <ptphuong11@cmcglobal.vn>
2022-12-30 08:46:57 +0700

    update

---
+++ cypress/e2e/Practice Form.cy.js
@@ -0,0 +1,50 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+})
+
+describe('Practice', () => {
+    it.only('Practice Form',()=>{
+        cy.viewport(1920,1080)
+        cy.visit('https://demoqa.com/automation-practice-form/')
+        cy.get("input[id='firstName']").type('Jen')
+        cy.get("input[id='lastName']").type('Pham')
+        cy.get("input[id='userEmail']").type('jenpham@gmail.com')
+        cy.get("input[id='gender-radio-2']").click({force:true})
+        cy.get("input[id='userNumber']").type('0334800444')
+        cy.get("input[id='dateOfBirthInput']").click()
+        cy.get("select[class='react-datepicker__year-select']").select('1993')
+        cy.get("select[class='react-datepicker__month-select']").select('4')
+        cy.get("div[class='react-datepicker__day react-datepicker__day--027']").click()
+        cy.get("div[class='subjects-auto-complete__value-container subjects-auto-complete__value-container--is-multi css-1hwfws3']").click().type('English{enter}')
+        cy.get("input[id='hobbies-checkbox-2']").click({force:true})
+        cy.get("textarea[id='currentAddress']").type('No 9, Duy Tan Street, Dich Vong Hau, Cau Giay, Ha Noi')
+        cy.get("div[id='state']").type('NCR{enter}')
+        cy.get("div[id='city']").type('Noida{enter}')
+        cy.get("button[id='submit']").click()
+})    
+
+    it('Nopcommerce',()=>{
+        cy.viewport(1902,1080)
+        cy.visit('https://demo.nopcommerce.com/')
+        cy.get("a[class='ico-register']").click()
+        cy.get("input[id='gender-female']").click()
+        cy.get("input[id='FirstName']").type('Jen')
+        cy.get("input[id='LastName']").type('Pham')
+        cy.get("select[name='DateOfBirthDay']").select('27')
+        cy.get("select[name='DateOfBirthMonth']").select('5')
+        cy.get("select[name='DateOfBirthYear']").select('1993')
+        cy.get("input[id='Email']").type('jenpham4@gmail.com')
+        cy.get("input[id='Company']").type('CMC Global')
+        cy.get("input[id='Password']").type('12345678')
+        cy.get("input[id='ConfirmPassword']").type('12345678')
+        cy.get("button[id='register-button']").click()
+        cy.get("div[class='buttons']").click()
+        cy.get("a[class='ico-login']").click()
+        cy.get("input[id='Email']").type('jenpham4@gmail.com')
+        cy.get("input[id='Password']").type('12345678')
+        cy.get("input[id='RememberMe']").click()
+        cy.get("button[class='button-1 login-button']").click()
+    })
+})
